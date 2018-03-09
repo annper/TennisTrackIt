b6df6ed2e9f08dc33ec95bf0d2332a3aca3c9e9a
APersson <annie.mc.persson@hotmail.com>
2018-03-09 15:38:05 +0000

    added category property to skill model

--- TennisTrackIt/DataManagers/Models/Skill.swift
+++ TennisTrackIt/DataManagers/Models/Skill.swift
@@ -9,12 +9,28 @@
 import Foundation
 import ObjectMapper
 
+class SkillList: Mappable {
+  
+  public var skills: [Skill] = []
+  
+  // MARK: - Mappable
+  
+  public required convenience init?(map: Map) {
+    self.init()
+  }
+  
+  public func mapping(map: Map) {
+    skills <- map["skills"]
+  }
+}
+
 class Skill: Mappable {
   
   public var id: Int = 0
   public var title: String = ""
   public var tags: [Tag] = []
   public var description: String?
+  public var category: SkillCategory = .other
   
   // MARK: - Mappable
   
@@ -27,6 +43,18 @@ class Skill: Mappable {
     title <- map["title"]
     tags <- map["tags"]
     description <- map["description"]
+    category <- (map["category"], EnumTransform<SkillCategory>())
   }
   
+}
+
+enum SkillCategory: String {
+  case footwork = "Footwork"
+  case tactics = "Tactics"
+  case groundstrokes = "Groundstrokes"
+  case volleys = "Volleys"
+  case serves = "Serves"
+  case  ros = "Return of serve"
+  case special = "Speciality shots"
+  case other = "Other"
 }
