7893bcf2c5dc1c96dd4e4f512b3d2196b6a24520
APersson <annie.mc.persson@hotmail.com>
2018-03-09 17:07:39 +0000

    section setup wip

--- TennisTrackIt/Modules/Skills/SkillsTableVC.swift
+++ TennisTrackIt/Modules/Skills/SkillsTableVC.swift
@@ -12,8 +12,10 @@ class SkillsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UI
   
   // MARK: - Private properties
   
+  private let skillDataManger = SkillDataManager()
   private let cellReuseIdentifier: String = "SkillCell"
-  private var allSkills: [Skill] = []
+  private var categories: [String] = []
+  private var allSkills: [[Skill]] = [[]]
   private var searchActive: Bool = false
   
   // MARK: - IBOutlets
@@ -41,7 +43,26 @@ class SkillsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UI
   override func viewDidLoad() {
     super.viewDidLoad()
     
+//    let skill = Skill()
+//
+//    skill.category = .footwork
+//    skillDataManger.add(skill)
+//
+//    skill.category = .groundstrokes
+//    skillDataManger.add(skill)
+//
+//    skill.category = .groundstrokes
+//    skillDataManger.add(skill)
+//
+//    skill.category = .ros
+//    skillDataManger.add(skill)
+//
+//    skill.category = .ros
+//    skillDataManger.add(skill)
+    
     navigationController?.navigationBar.hideShadow()
+    
+    loadSavedSkills()
   }
   
   override func didReceiveMemoryWarning() {
@@ -56,6 +77,32 @@ class SkillsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UI
     Logger.info("Skills tab")
   }
   
+  // MARK: - Private methods
+  
+  private func loadSavedSkills() {
+    
+    guard let savedSkills = skillDataManger.savedSkills() else {
+      return
+    }
+    
+    categories = removeDuplictes(savedSkills.skills.map({ $0.category.rawValue }))
+    let sortedSkills = [savedSkills.skills.sorted { $0.category.rawValue < $1.category.rawValue }]
+    allSkills = sortedSkills//savedSkills.skills
+    
+    
+  }
+  
+  func removeDuplictes(_  array: [String]) -> [String] {
+    return array.reduce([]) { (result, category) -> [String] in
+      return result.contains(category) ? result : result + [category]
+//      if !result.contains(category) {
+//        return result + [category]
+//      } else {
+//        return result
+//      }
+    }
+  }
+  
   /*
    // MARK: - Navigation
    
@@ -68,45 +115,14 @@ class SkillsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UI
   
   // MARK: - UISearchBarDelegate
   
-//  func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
-//    searchActive = true;
-//  }
-//
-//  func searchBarTextDidEndEditing(searchBar: UISearchBar) {
-//    searchActive = false;
-//  }
-//
-//  func searchBarCancelButtonClicked(searchBar: UISearchBar) {
-//    searchActive = false;
-//  }
-//
-//  func searchBarSearchButtonClicked(searchBar: UISearchBar) {
-//    searchActive = false;
-//  }
-  
-//  func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
-  
-//    filtered = data.filter({ (text) -> Bool in
-//      let tmp: NSString = text
-//      let range = tmp.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
-//      return range.location != NSNotFound
-//    })
-//    if(filtered.count == 0){
-//      searchActive = false;
-//    } else {
-//      searchActive = true;
-//    }
-//    self.tableView.reloadData()
-//  }
-  
   // MARK: - TableViewDataSource
   
   func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
+    return categories.count //1
   }
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 3
+    return allSkills[section].count//3
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
