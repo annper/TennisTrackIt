68f187f85cfb85e4885c54a294daffce93c2fe3c
APersson <annie.mc.persson@hotmail.com>
2018-03-09 14:48:22 +0000

    setup section header and tableview footer for skills tab

--- TennisTrackIt/Modules/Skills/SkillsTableVC.swift
+++ TennisTrackIt/Modules/Skills/SkillsTableVC.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class SkillsTableVC: UITableViewController {
+class SkillsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+  
+  // MARK: - Private properties
+  
+  private let cellReuseIdentifier: String = "SkillCell"
+  private var allSkills: [Skill] = []
+  private var searchActive: Bool = false
+  
+  // MARK: - IBOutlets
+  
+  @IBOutlet var tableView: UITableView! { didSet {
+    tableView.tableFooterView = UIView()
+  }}
+  
+  @IBOutlet var searchBar: UISearchBar! { didSet {
+    searchBar.layer.borderWidth = 2
+    searchBar.layer.borderColor = UIColor(red: 0.400, green: 0.800, blue: 1.000, alpha: 1).cgColor
+  }}
+  
+  // MARK: - IBActions
+  
+  @IBAction func didTapSettingBarButtonItem(_ sender: UIBarButtonItem) {
+    Logger.info("didTapSettingBarButtonItem")
+  }
+  
+  @IBAction func didTapCreateBarButtonItem(_ sender: UIBarButtonItem) {
+    Logger.info("didTapCreateBarButtonItem")
+  }
   
   // MARK: - UITableViewController
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
   }
   
   override func didReceiveMemoryWarning() {
@@ -29,61 +56,6 @@ class SkillsTableVC: UITableViewController {
     Logger.info("Skills tab")
   }
   
-  // MARK: - Table view data source
-  
-  override func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
-  }
-  
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 3
-  }
-  
-  /*
-   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-   let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-   
-   // Configure the cell...
-   
-   return cell
-   }
-   */
-  
-  /*
-   // Override to support conditional editing of the table view.
-   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the specified item to be editable.
-   return true
-   }
-   */
-  
-  /*
-   // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-   if editingStyle == .delete {
-   // Delete the row from the data source
-   tableView.deleteRows(at: [indexPath], with: .fade)
-   } else if editingStyle == .insert {
-   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-   }
-   }
-   */
-  
-  /*
-   // Override to support rearranging the table view.
-   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-   
-   }
-   */
-  
-  /*
-   // Override to support conditional rearranging of the table view.
-   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the item to be re-orderable.
-   return true
-   }
-   */
-  
   /*
    // MARK: - Navigation
    
@@ -93,5 +65,87 @@ class SkillsTableVC: UITableViewController {
    // Pass the selected object to the new view controller.
    }
    */
+  
+  // MARK: - Private methods
+  
+  
+  // MARK: - UISearchBarDelegate
+  
+//  func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+//    searchActive = true;
+//  }
+//
+//  func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+//    searchActive = false;
+//  }
+//
+//  func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+//    searchActive = false;
+//  }
+//
+//  func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+//    searchActive = false;
+//  }
+  
+//  func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+  
+//    filtered = data.filter({ (text) -> Bool in
+//      let tmp: NSString = text
+//      let range = tmp.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
+//      return range.location != NSNotFound
+//    })
+//    if(filtered.count == 0){
+//      searchActive = false;
+//    } else {
+//      searchActive = true;
+//    }
+//    self.tableView.reloadData()
+//  }
+  
+  // MARK: - TableViewDataSource
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 3
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+    
+    return cell
+  }
+  
+  // MARK: - UITableViewDelegate
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 60
+  }
+  
+  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return 50
+  }
+  
+  func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+    
+    guard let header = view as? UITableViewHeaderFooterView else {
+      return
+    }
+    
+    header.textLabel?.textColor = UIColor(red: 0.459, green: 0.459, blue: 0.459, alpha: 1)
+    header.textLabel?.font = UIFont(name: "HelveticaNeue-Regular", size: 16.0)
+    
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    Logger.info("Selected row at indexPath: \(indexPath)")
+  }
+  
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return "Header title"
+  }
   
 }
