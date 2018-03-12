cb5b6826c276be9ededbc545645684ed95700afa
APersson <annie.mc.persson@hotmail.com>
2018-03-12 15:08:01 +0000

    setup SkillDetail UI + VC

---
+++ TennisTrackIt/Modules/Skills/SkillDetail/SkillDetailVC.swift
@@ -0,0 +1,25 @@
+//
+//  SkillDetailVC.swift
+//  TennisTrackIt
+//
+//  Created by Annie Persson on 12/03/2018.
+//  Copyright © 2018 Annie Persson. All rights reserved.
+//
+
+import UIKit
+
+class SkillDetailVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    Logger.info("SkillDetailVC")
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    
+    Logger.warn("didReceiveMemoryWarning")
+  }
+  
+}
